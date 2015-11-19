0e0a3ab2aad48f453300bcabbf294d5714bdebf0
Philip Deegan <philip.deegan@gmail.com>
2015-11-19 13:31:00 +0000

    Create clang_3.7_x86_64_RELEASE_BASIC.sh

---
+++ clang_3.7_x86_64_RELEASE_BASIC.sh
@@ -0,0 +1,17 @@
+
+DIR=
+mkdir -p $DIR
+svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_371/rc1 llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_371/rc1/ clang
+cd ../..
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_371/rc1/ extra
+cd ../../../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_371/rc1/ compiler-rt
+cd ../..
+mkdir build
+cd build
+../llvm/configure --prefix=$DIR --enable-optimized1> /dev/null
+make -j 4 1> /dev/null
+make install 1> /dev/null
