74d79ee6549373b57c629ae1317f81378dce2269
dekken <philip.deegan@gmail.com>
2017-02-12 21:53:01 +0000

    clang trunk script

---
+++ clang_trunk.sh
@@ -0,0 +1,20 @@
+
+set -e
+OUT=/opt/chain/clang
+THREADS=20
+mkdir -p $OUT
+# svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+# cd llvm/tools
+# svn co http://llvm.org/svn/llvm-project/cfe/trunk/ clang
+# cd ../..
+# cd llvm/tools/clang/tools
+# svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk/ extra
+# cd ../../../..
+# cd llvm/projects
+# svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk/ compiler-rt
+# cd ../..
+# mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$OUT ../llvm 1> /dev/null
+make -j$THREADS 1> /dev/null
+make install 1> /dev/null
