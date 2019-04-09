33576daacea9954abafa5e9c4df7ea8c5a35dce8
dekken <philip.deegan@gmail.com>
2019-04-09 20:52:10 +0200

    .

---
+++ clang8.sh
@@ -0,0 +1,22 @@
+
+TAG="RELEASE_800"
+
+set -e
+OUT=/opt/chain/clang8
+THREADS=6
+mkdir -p $OUT
+svn co http://llvm.org/svn/llvm-project/llvm/tags/${TAG}/final llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/tags/${TAG}/final/ clang
+cd ../..
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/tags/${TAG}/final/ extra
+cd ../../../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/${TAG}/final/ compiler-rt
+cd ../..
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$OUT ../llvm 1> /dev/null
+make -j$THREADS 1> /dev/null
+make install 1> /dev/null
